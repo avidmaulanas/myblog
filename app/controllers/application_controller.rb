4381dd3dad4d20c2b776c15927b67907ce7495af
avidmaulanas <avidmaulanas@gmail.com>
2015-03-01 02:19:06 +0700

    modified: app/controllers/application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,16 +10,16 @@ class ApplicationController < ActionController::Base
 
   before_action :check_subdomain, unless: :devise_controller?
 
-  after_filter :set_csrf_cookie_for_ng
+  # after_filter :set_csrf_cookie_for_ng
 
-  def set_csrf_cookie_for_ng
-    cookies['XSRF-TOKEN'] = form_authenticity_token if protect_against_forgery?
-  end
+  # def set_csrf_cookie_for_ng
+  #   cookies['XSRF-TOKEN'] = form_authenticity_token if protect_against_forgery?
+  # end
 
-  protected
-    def verified_request?
-      super || valid_authenticity_token?(session, request.headers['X-XSRF-TOKEN'])
-    end
+  # protected
+  #   def verified_request?
+  #     super || valid_authenticity_token?(session, request.headers['X-XSRF-TOKEN'])
+  #   end
 
   private
     def configure_devise_params
