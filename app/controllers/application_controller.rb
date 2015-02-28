61cb046663b34ac5a3632dfb17dc92920b9371aa
avidmaulanas <avidmaulanas@gmail.com>
2015-03-01 05:10:32 +0700

    modified: app/controllers/application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -36,7 +36,7 @@ class ApplicationController < ActionController::Base
 
     def after_sign_in_path_for(resource)
       if current_user.respond_to?("username")
-        dashboard_url(:subdomain => current_user.username)
+        dashboard_url(:subdomain => "#{current_user.username}.#{request.subdomain}")
       end      
     end
 
