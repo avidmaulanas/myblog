5d6b905fafbf7466809549d9cc9b230e9a3259e4
avidmaulanas <avidmaulanas@gmail.com>
2015-03-01 01:41:28 +0700

    modified: config/environments/production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,4 +79,19 @@ Rails.application.configure do
 
   # less
   config.less.compress = true
+
+  config.action_mailer.default_url_options = { :host => 'whispering-wave-3076.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "whispering-wave-3076.herokuapp.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV["GMAIL_USERNAME"],
+    password: ENV["GMAIL_PASSWORD"]
+  }
 end
