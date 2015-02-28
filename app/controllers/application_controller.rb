8670686a12309d06a0f198fad9cb9d7f033474b9
avidmaulanas <avidmaulanas@gmail.com>
2015-03-01 03:31:40 +0700

    modified: application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,27 +1,27 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  # protect_from_forgery with: :exception
 
   before_action :authenticate_user!  
-  before_action :set_current_user
+  before_filter :set_current_user
 
   before_filter :configure_devise_params, if: :devise_controller?
 
   # before_action :check_subdomain, unless: :devise_controller?
 
-  # protect_from_forgery
+  protect_from_forgery
 
-  # after_filter :set_csrf_cookie_for_ng
+  after_filter :set_csrf_cookie_for_ng
 
-  # def set_csrf_cookie_for_ng
-  #   cookies['XSRF-TOKEN'] = form_authenticity_token if protect_against_forgery?
-  # end
+  def set_csrf_cookie_for_ng
+    cookies['XSRF-TOKEN'] = form_authenticity_token if protect_against_forgery?
+  end
 
-  # protected
-  #   def verified_request?
-  #     super || valid_authenticity_token?(session, request.headers['X-XSRF-TOKEN'])
-  #   end
+  protected
+    def verified_request?
+      super || valid_authenticity_token?(session, request.headers['X-XSRF-TOKEN'])
+    end
 
   private
     def configure_devise_params
