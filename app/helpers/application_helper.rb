dcd0317ec658a911fbb838fbd37f4ac888eed72c
rifqi669 <rifqi@41studio.com>
2015-03-03 13:59:33 +0700

    make better

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,27 +1,11 @@
 module ApplicationHelper
 	include ActsAsTaggableOn::TagsHelper
-	
-	# def alert_tag(message = nil, type = 'info')
-	# 	if message 
-
-	# 		html = <<-HTML
-	#     <div class="alert alert-#{type} alert-dismissible fade in" role="alert"> <button type="button"
-	#     class="close" data-dismiss="alert" aria-label="close">x</button>
-	#       #{message}
-	#     </div>
-	#     HTML
-
-	#     html.html_safe
-	# 	end
-	# end
-
-  
 
   def flash_messages(options = {})    
     flash.each do |msg_type, message|
       content = 
       content_tag(:div, message, class: "alert #{bootstrap_class_for(msg_type)} fade in", role: "alert") do 
-        concat content_tag(:button, content_tag(:span, "&times;".html_safe, "aria-hidden": "true"), class: "close", data: { dismiss: 'alert'}, "aria-label": "Close" )
+        concat content_tag(:button, content_tag(:span, "&times;".html_safe, aria_hidden: "true"), class: "close", data: { dismiss: 'alert'}, aria_label: "Close" )
         concat message 
       end
 
@@ -46,7 +30,7 @@ module ApplicationHelper
 	end
 
   private 
-  def bootstrap_class_for(flash_type)
-    { success: "alert-success", error: "alert-danger", alert: "alert-warning", notice: "alert-info" }[flash_type.to_sym] || flash_type.to_s
-  end
+    def bootstrap_class_for(flash_type)
+      { success: "alert-success", error: "alert-danger", alert: "alert-warning", notice: "alert-info" }[flash_type.to_sym] || flash_type.to_s
+    end
 end
