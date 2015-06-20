ec965b380c39d0fa54f37f4baabc96913088a0a8
avid <avid@41studio.com>
2015-06-20 22:12:46 +0700

    change route for login and logout

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,13 @@ Rails.application.routes.draw do
 
   root 'home#index'
 
-  devise_for :users
+  devise_for :users, skip: [:sessions]
+
+  as :user do
+    get 'login' => 'devise/sessions#new', as: :new_user_session
+    post 'login' => 'devise/sessions#create', as: :user_session
+    delete 'logout' => 'devise/sessions#destroy', as: :destroy_user_session
+  end
 
   post 'rate' => 'articles#rate'
 
