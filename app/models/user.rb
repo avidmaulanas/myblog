28fc6b11f374c3d44b0bb0a1489af63e6044f635
avid <avid@41studio.com>
2015-07-30 21:01:43 +0700

    fix slug

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,18 +4,23 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable,
+         :confirmable
 
  	has_many :articles
 
- 	validates :email, :username, presence: true, uniqueness: true
-  validates :firstname, :lastname, presence: true
+  validates :email, presence: true, uniqueness: true
+ 	validates :password, presence: true
+  validates :firstname, :lastname, presence: true, on: :update
 
  	mount_uploader :avatar, AvatarUploader
   crop_uploaded :avatar
   process_in_background :avatar
 
+  before_create :set_slug
+
   extend FriendlyId
+
   friendly_id :username, use: [:slugged, :finders]
 
  	scope :recent, ->(n) { order("created_at DESC").limit(n) }
@@ -30,6 +35,18 @@ class User < ActiveRecord::Base
 
   def full_name
     "#{firstname} #{lastname}"
+  end
+
+  def set_slug
+    self.slug = [*('a'..'z'),*('0'..'9')].shuffle[0,8].join
+  end
+
+  def should_generate_new_friendly_id?
+    if !slug? || username_changed?
+      true
+    else
+      false
+    end
   end
 
 end
