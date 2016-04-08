c3252e33c8d4b33fed5561ac93e4c79beff15afd
Avid Maulana Saputra <avidmaulanas@gmail.com>
2016-04-09 00:47:47 +0700

    Remove mailer config

--- config/environment.rb
+++ config/environment.rb
@@ -3,19 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
-
-# Setting Email Method
-Rails.application.configure do
-	config.action_mailer.delivery_method = :smtp
-
-	config.action_mailer.smtp_settings = {
-		address: "smtp.gmail.com",
-		port: 587,
-		domain: "41studio.com",
-		authentication: "plain",
-		user_name: "avid",
-		password: "ur4ngAsup",
-		enable_starttls_auto: true
-	}
-	
-end
